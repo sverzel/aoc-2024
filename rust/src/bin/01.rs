601f8b5cc8789d2cc889874af34d76426849b812
sverzel <stefan@verzel.se>
2024-12-02 22:00:25 +0100

    automatically excluded now included

---
+++ rust/src/bin/01.rs
@@ -0,0 +1,62 @@
+advent_of_code::solution!(1);
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let mut left = vec!();
+    let mut right = vec!();
+    for line in input.split("\n") {
+        let value: Vec<&str> = line.split_whitespace().collect();
+        if value.len() > 0 {
+            left.push(value[0].parse::<i32>().unwrap());
+            right.push(value[1].parse::<i32>().unwrap());
+        }
+    }
+
+    left.sort();
+    right.sort();
+
+    let mut sum: u32 = 0;
+    for i in 0 .. left.len() {
+        sum += (left[i] - right[i]).abs() as u32;
+    }
+
+    Some(sum)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let mut left = vec!();
+    let mut right = vec!();
+    for line in input.split("\n") {
+        let value: Vec<&str> = line.split_whitespace().collect();
+        if value.len() > 0 {
+            left.push(value[0].parse::<i32>().unwrap());
+            right.push(value[1].parse::<i32>().unwrap());
+        }
+    }
+
+    left.sort();
+    right.sort();
+
+    let mut sum: u32 = 0;
+    for l in left {
+        sum += (l * (&right).into_iter().filter(|&v| *v == l).count() as i32) as u32;
+    }
+
+    Some(sum)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
---
+++ rust/src/bin/02.rs
@@ -0,0 +1,66 @@
+advent_of_code::solution!(2);
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let mut safe: u32 = 0;
+
+    for line in input.split("\n") {
+        let values: Vec<i32> = line.split_whitespace().map(|v| v.parse::<i32>().unwrap()).collect();
+        let mut a = values.clone();
+        a.sort_by(|a, b| a.cmp(b));
+        let mut d = values.clone();
+        d.sort_by(|a, b| b.cmp(a));
+
+        if values.len() > 0 {
+            if (0 .. values.len()-1).into_iter().map(|i| (values[i] - values[i+1]).abs() ).filter(|&i| i > 0 && i < 4 ).count() == values.len()-1
+                && (values.eq(&a) || values.eq(&d)) {
+                safe += 1;
+            }
+        }
+    }
+
+    Some(safe)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let mut safe: u32 = 0;
+
+    for line in input.split("\n") {
+        let v: Vec<i32> = line.split_whitespace().map(|v| v.parse::<i32>().unwrap()).collect();
+
+        for i in 0 .. v.len() {
+            let mut values = v.clone();
+            values.remove(i);
+            let mut a = values.clone();
+            a.sort_by(|a, b| a.cmp(b));
+            let mut d = values.clone();
+            d.sort_by(|a, b| b.cmp(a));
+
+            if values.len() > 0 {
+                if (0 .. values.len()-1).into_iter().map(|i| (values[i] - values[i+1]).abs() ).filter(|&i| i > 0 && i < 4 ).count() == values.len()-1
+                    && (values.eq(&a) || values.eq(&d)) {
+                    safe += 1;
+                    break;
+                }
+            }
+        }
+    }
+
+    Some(safe)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
